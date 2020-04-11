dbbf00f9678277bacc54b495e11dc15f6c2b3882
RaulCesar-programming <raulceasds@gmail.com>
2020-04-11 18:59:58 -0400

    Novas incrementações e melhorias.

--- src/js/script.js
+++ src/js/script.js
@@ -17,7 +17,11 @@ const elements = {
     c: document.querySelector('input#c'),
 
     hipoLabel: document.querySelector('label.hipo-label'),
-    resList: document.querySelector('ul.res')
+    resList: document.querySelector('ul.res'),
+
+    titA: document.querySelector('span#inputTitA'),
+    titB: document.querySelector('span#inputTitB'),
+    titC: document.querySelector('span#inputTitC'),
     
 }
 
@@ -32,16 +36,61 @@ function update()
         elements.sides.classList.remove('hidde')
         
 
-    } else{
+    } else if(elements.opTypes.value == 'tg'){
+        elements.hipoLabel.classList.add('hidde')
+        elements.de.classList.remove('hidde')
+    }
+    else{
         elements.de.classList.remove('hidde')
         elements.hipoLabel.classList.remove('hidde')
 
 
     }
+
+    //titulos
+
+    if(elements.opTypes.value == 'cos' || elements.opTypes.value == 'sen')
+    {
+        switch(elements.opOper.value)
+        {
+            case 'beta':
+                elements.titA.innerHTML = 'Cateto Oposto'
+                elements.titB.innerHTML = 'Cateto adjacente'
+                elements.titC.innerHTML = 'Hipotenusa'
+            break;
+            case 'alfa':
+                elements.titA.innerHTML = 'Cateto Adjacente'
+                elements.titB.innerHTML = 'Cateto Oposto'
+                elements.titC.innerHTML = 'Hipotenusa'
+        }
+    } else if(elements.opTypes.value == 'tg'){
+        switch(elements.opOper.value)
+        {
+            case 'beta':
+                elements.titA.innerHTML = 'Cateto Oposto'
+                elements.titB.innerHTML = 'Cateto adjacente'
+            break;
+            case 'alfa':
+                elements.titA.innerHTML = 'Cateto Adjacente'
+                elements.titB.innerHTML = 'Cateto Oposto'
+        }
+    }
+    else {
+        elements.titA.innerHTML = 'Lado A'
+        elements.titB.innerHTML = 'Lado B'
+    }
+    
 }
 
 function calculo()
 {
+    if(elements.a.value == '' || elements.h.value == '' || elements.c.value == '')
+    {
+        alert('Preencha todas as entradas')
+    }
+    else
+    {
+
     let res = {
         cos: 0,
         sen: 0,
@@ -51,7 +100,7 @@ function calculo()
     if(elements.opTypes.value == 'hipo')
     {
         //pegar valor dos lados
-        res.hipo = Math.sqrt(Math.pow(Number(elements.a.value), 2) + Math.pow(Number(elements.c.value), 2))
+        res.hipo = Math.sqrt(Math.pow(Number(elements.a.value), 2) + Math.pow(Number(elements.c.value), 2)).toFixed(2)
         results.push(`Hipotenusa: ${res.hipo}`)
         resAdd()
         console.log(res.hipo)
@@ -60,6 +109,7 @@ function calculo()
 
     if(elements.opTypes.value == 'sen')
     {
+
         //pegar valor dos lados
         let ang = elements.opOper.value
         let op = null
@@ -70,29 +120,72 @@ function calculo()
         {
             case 'beta':
                 op = Number(elements.a.value) 
-                adj = Number(elements.c.value)
                 qu = '(beta)'
             break;
             case 'alfa':
                 op = Number(elements.c.value)
                 adj = Number(elements.a.value)
                 qu = '(alfa)'
+
         }
 
         res.sen = op/hip
         results.push(`Seno${qu}: ${res.sen}`)
-        resAdd('Seno')
+        resAdd()
         //calcular seno
     }
     if(elements.opTypes.value == 'cos')
     {
-        //pegar valor dos angulos
+        //pegar valor dos lados
+        let ang = elements.opOper.value
+        let op = null
+        let adj = null
+        let hip = elements.h.value
+        let qu = ''
         //calcular coscenos
+        switch(ang){
+            case 'beta':
+                adj = Number(elements.c.value)
+                qu = '(beta)'
+            break;
+            case 'alfa':
+                adj = Number(elements.a.value)
+                qu = '(alfa)'
+        }
+
+        res.cos = adj/hip
+        results.push(`Cosseno${qu}: ${res.cos}`)
+        resAdd()
     }
     if(elements.opTypes.value == 'tg')
     {
-        //pegar valor dos angulos
+         //pegar valor dos lados
+         let ang = elements.opOper.value
+         let op = null
+         let adj = null
+         let hip = elements.h.value
+         let qu = ''
+         switch(ang)
+         {
+             case 'beta':
+                 op = Number(elements.a.value) 
+                 adj = Number(elements.c.value)
+                 qu = '(beta)'
+             break;
+             case 'alfa':
+                 op = Number(elements.c.value)
+                 adj = Number(elements.a.value)
+                 qu = '(alfa)'
+ 
+         }
+ 
+         res.tg = op/adj
+         results.push(`Tangente${qu}: ${res.tg}`)
+         resAdd()
+         //calcular seno
+        
         //calcular coscenos
+    }
     }
 }
 
