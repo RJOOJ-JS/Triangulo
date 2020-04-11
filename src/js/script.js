19d539d957a36ba91760b0c8799a3fb6d1fdc3a7
RaulCesar-programming <raulceasds@gmail.com>
2020-04-11 14:11:32 -0400

    Atualiuzação

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,10 @@
-const inputs = {
+const elements = {
     //header selects
     opTypes: document.querySelector('select#operacoes'),
     opOper: document.querySelector('select#operar'),
     de: document.querySelector('div.de'),
 
-    //other inputs
+    //other elements
     beta: document.querySelector('input#beta'),
     alfa: document.querySelector('input#alfa'),
     hipo: document.querySelector('input#hipo'),
@@ -19,18 +19,49 @@ const inputs = {
 }
 function update()
 {
-    if(inputs.opTypes.value == 'hipo')
+    if(elements.opTypes.value == 'hipo')
     {
-        inputs.de.classList.add('hidde')
-        inputs.angles.classList.add('hidde')
-        inputs.sides.classList.remove('hidde')
+        elements.de.classList.add('hidde')
+        elements.angles.classList.add('hidde')
+        elements.sides.classList.remove('hidde')
 
     } else{
-        inputs.de.classList.remove('hidde')
-        inputs.sides.classList.add('hidde')
-        inputs.angles.classList.remove('hidde')
+        elements.de.classList.remove('hidde')
+        elements.sides.classList.add('hidde')
+        elements.angles.classList.remove('hidde')
 
     }
 }
 
+function calculo()
+{
+    let res = {
+        cos: 0,
+        sen: 0,
+        tg: 0,
+        hipo: 0,
+    }
+    if(elements.opTypes.value == 'hipo')
+    {
+        //pegar valor dos lados
+        res.hipo = Math.sqrt((Math.pow(Number(elements.a.value), 2)+ Math.pow(Number(elements.c.value))))
+        console.log(res.hipo)
+        //fazer calculo
+    }
 
+    if(elements.opTypes.value == 'sen')
+    {
+        //pegar valor dos angulos
+        //calcular seno
+    }
+    if(elements.opTypes.value == 'cos')
+    {
+        //pegar valor dos angulos
+        //calcular coscenos
+    }
+    if(elements.opTypes.value == 'tg')
+    {
+        //pegar valor dos angulos
+        //calcular coscenos
+    }
+}
